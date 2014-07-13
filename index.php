1d5ad3c5d07c5191e7e9e9c196eb9f9f8b26791c
Weiyu Qi <qi@weiyu.me>
2014-07-13 23:44:11 +0800

    use ecoder

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <?php
 
-// change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+/* 
+main page - include all items, in iframes ##
+*/
 
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+include "code.php"; // single included settings file ##
+include "code/base/header.php"; // html header, branding ##
 
-require_once($yii);
-Yii::createWebApplication($config)->run();
+    // build main structure ##
+    echo '
+    </head>
+    <body>';
+    
+    // test if javascript enabled ##
+    //include "code/base/loader.php"; 
+        
+        echo '
+        <div id="left">
+            <iframe name="tree" id="iframe_tree" src="tree.php"></iframe>
+        </div>
+
+        <div id="content">';
+
+            // include required tabs mark-up ##
+            include "code/tabs/build.php";                
+            
+        echo '
+        </div>';
+            
+    include "code/tabs/init.php"; // build tabs ##
+
+    // notes and messages ##
+    include "code/base/notes.php";
+
+include "code/base/footer.php"; // html footer ##
+
+?>
